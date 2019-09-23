7be2ffc18811c5205037266cd35790e95d1d522e
MickeyRider01 <s5935512082@phuket.psu.ac.th>
2019-09-23 17:08:57 +0700

    Commit Final

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import Content from './Content';
 import CharacterCard from './CharacterCard';
 import WordCard from './WordCard'
 let x = 10
-const word = "Hello";
+const word = "MIkael";
 class App extends Component {
   render() {
     return (
+      <div classname="Head">
+        <h1>Card Game</h1>
+        <h2>created by Mikael Yapa 5935512082</h2>
       <div className="App">
         <div className="card">
-          <WordCard value="hello"/>
+          <WordCard value="Mikael"/>
         </div>
+      </div>
       </div>
  );
  }
